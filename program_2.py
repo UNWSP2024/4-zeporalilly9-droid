26a6b1a1729cd09a498e7ead6ade858048652f8b
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-26 20:20:23 -0500

    program 2
    week 4

--- program_2.py
+++ program_2.py
@@ -3,23 +3,24 @@
 # tickets are desired for each movie.  
 # At the end of the program it prints out the total number of tickets desired by the user.  
 # Use either a "for loop" or "while loop" to accomplish this.
-#Author: Zepora
-#Date: 9/26/2025
-#Title: Movie Ticket Counter
+# Program #2: Movie Tix
+# Author: Zepora
+# Date: 9/26/2025
+# Title: Movie Ticket Counter
 
 def main():
     # Initialize total ticket counter
     total_tickets = 0
-
+    
     # Ask how many movies they want to buy tickets for
     num_movies = int(input("How many different movies are you buying tickets for? "))
-
+    
     # Loop through each movie entry
     for i in range(num_movies):
         movie_name = input(f"Enter the name of movie {i + 1} :")
         tickets = int(input(f"How many tickets for '{movie_name}'? "))
         total_tickets += tickets
-
+    
     # Display the total number of tickets
     print(f"\nTotal tickets purchased: {total_tickets}")
 
