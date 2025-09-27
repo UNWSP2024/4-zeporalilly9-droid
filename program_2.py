310caedfd57f67ead3b16d24e47947da6b307cc9
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-26 21:01:00 -0500

    # Program #2: Movie Tix
    # Program #2: Movie Tix week 4

--- program_2.py
+++ program_2.py
@@ -1,10 +1,4 @@
 # Program #2: Movie Tix
-# Write a program that has the user input various movie names and how many 
-# tickets are desired for each movie.  
-# At the end of the program it prints out the total number of tickets desired by the user.  
-
-# Use either a "for loop" or "while loop" to accomplish this.
-# Program #2: Movie Tix
 # Author: Zepora
 # Date: 9/26/2025
 # Title: Movie Ticket Counter
