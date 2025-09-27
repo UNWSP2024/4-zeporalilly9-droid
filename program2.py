5c10ba7b234c5b45af93f69cb5e54e49bc9b141e
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-26 23:14:01 -0500

    program 2

--- program2.py
+++ program2.py
@@ -9,9 +9,9 @@ total_tickets = 0
 num_movies = int(input("How many movies do you want to enter? "))
 
 for i in range(num_movies):
-    movie = input("Total number of tickets desired: 5 ")
+    
+    movie = input("Enter the movie name: ")
     tickets = int(input(f"How many tickets for {movie}? "))
     total_tickets += tickets
-
-print("Total number of tickets desired:", total_tickets)
-
+    
+    print("Total number of tickets desired:", total_tickets)
