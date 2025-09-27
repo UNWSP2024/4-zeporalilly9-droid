42aa32b42d0271467ea2865ac425989c534ffbda
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-26 21:55:41 -0500

    Program #2: Movie Tix, Program #3: Average Rainfall, Program #5: Bank Balance
    all week 4 programs

---
+++ program2.py
@@ -0,0 +1,17 @@
+# Program #2: Movie Tix
+# Author: Zepora
+# Date: 9/26/2025
+# Title: Movie Ticket Counter
+
+# Program #2: Movie Tix
+
+total_tickets = 0
+num_movies = int(input("How many movies do you want to enter? "))
+
+for i in range(num_movies):
+    movie = input("Total number of tickets desired: 5 ")
+    tickets = int(input(f"How many tickets for {movie}? "))
+    total_tickets += tickets
+
+print("Total number of tickets desired:", total_tickets)
+
---
+++ program3.py
@@ -0,0 +1,22 @@
+# Program #3: Average Rainfall
+#Author: Zepora
+#Date: 9/26/2025
+#Title: Average Rainfall Calculator
+
+years = int(input("Enter number of years: "))
+
+total_rainfall = 0
+total_months = years * 12
+
+for year in range(1, years + 1):
+    print(f"Year {year}:")
+    for month in range(1, 13):
+        rainfall = float(input(f"  Enter rainfall (in inches) for month {month}: "))
+        total_rainfall += rainfall
+
+average_rainfall = total_rainfall / total_months
+
+print("\nResults:")
+print("Total months:", total_months)
+print("Total rainfall:", total_rainfall, "inches")
+print("Average rainfall per month:", average_rainfall, "inches")
