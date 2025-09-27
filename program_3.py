68656a911fc567a1056af5038f692c611c401be0
zeporalilly9-droid <zeporalilly9@gmail.com>
2025-09-26 21:26:07 -0500

    # Program #2: Movie Tix

--- program_3.py
+++ program_3.py
@@ -1,4 +1,8 @@
 # Program #3: Average Rainfall
+# Program #2: Movie Tix
+# Author: Zepora
+# Date: 9/26/2025
+# Title: Movie Ticket Counter
 # Write a program that uses nested loops to collect data and calculate the average 
 # rainfall over a period of years.  
 # The program should first ask for the number of years.  
@@ -7,12 +11,26 @@
 # Each iteration of the inner loop will ask the user for inches of rainfall for each month.  
 # After all iterations, the program should display the number of months, 
 # the total inches of rainfall, and the average rainfall per month for the entire period.
+# Write a program that has the user input various movie names and how many 
+# tickets are desired for each movie.  
+# At the end of the program it prints out the total number of tickets desired by the user.  
+# Use either a "for loop" or "while loop" to accomplish this.
 
 def main():
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################    
+    # Initialize total ticket counter
+    total_tickets = 0
 
+    # Ask how many movies they want to buy tickets for
+    num_movies = int(input("How many different movies are you buying tickets for? "))
+
+    # Loop through each movie entry
+    for i in range(num_movies):
+        movie_name = input(f"Enter the name of movie {i + 1}: ")
+        tickets = int(input(f"How many tickets for '{movie_name}'? "))
+        total_tickets += tickets
+
+    # Display the total number of tickets
+    print("Total number of tickets:", total_tickets)
 
 if __name__ == '__main__':
     main()
